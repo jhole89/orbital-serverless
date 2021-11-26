29fea977d66b8bbbd85529d123d6ef4db5c0136c
jhole89 <joel.lutman@gmail.com>
2021-11-25 16:27:46 -0800

    increase neptune timeouts

--- neptune.tf
+++ neptune.tf
@@ -4,6 +4,11 @@ resource "aws_neptune_cluster" "orbital" {
   skip_final_snapshot       = true
   apply_immediately         = true
   vpc_security_group_ids    = var.vpc_security_group_ids
+  timeouts {
+    create = "20m"
+    update = "20m"
+    delete = "20m"
+  }
 }
 
 resource "aws_neptune_cluster_instance" "orbital" {
@@ -12,10 +17,16 @@ resource "aws_neptune_cluster_instance" "orbital" {
   engine             = "neptune"
   instance_class     = "db.t3.medium"
   apply_immediately  = true
+  timeouts {
+    create = "20m"
+    update = "20m"
+    delete = "20m"
+  }
 }
 
 resource "aws_neptune_cluster_endpoint" "orbital" {
   cluster_identifier          = aws_neptune_cluster.orbital.id
   cluster_endpoint_identifier = lower(local.project_name)
   endpoint_type               = "ANY"
+
 }
